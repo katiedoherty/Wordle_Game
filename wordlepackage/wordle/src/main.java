0faed6d5e77f8350f0312306555b2d5f8d014b58
katiedoherty <katie.doherty@mycit.ie>
2022-02-04 19:53:16 +0000

    added in code to java project

---
+++ wordlepackage/wordle/src/main.java
@@ -0,0 +1,7 @@
+package wordlepackage.wordle.src;
+public class main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String intro = "Hello World";
+    }
+}
