ee23bb2acb67ae283b354bc8475d5fcdc00cf983
katiedoherty <katie.doherty@mycit.ie>
2022-02-06 19:30:25 +0000

    trying to configure gitpod with jframe

--- wordlepackage/wordle/src/main.java
+++
@@ -1,7 +0,0 @@
-package wordlepackage.wordle.src;
-public class main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        String intro = "Hello World";
-    }
-}
